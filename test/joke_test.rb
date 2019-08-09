f7cedf50b5fbfa3083719760745d6e0f8ad1f8c7
ian douglas <ian.douglas@iandouglas.com>
2019-08-09 09:56:28 -0600

    1908 2nd week challenge

---
+++ test/joke_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+
+class JokeTest < Minitest::Test
+  def test_it_exists
+  end
+
+  def test_it_has_attributes
+  end
+end
