0cc1ea4af275385d911b2eb724ceb27d93acea7e
Carleigh Crockett <capleugh@gmail.com>
2019-08-21 09:22:55 -0600

    Adds Joke class and JokeTest

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -1,11 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
+require 'pry'
 
 class JokeTest < Minitest::Test
+  def setup
+    @joke = Joke.new(1,"Why did the strawberry cross the road?", "Because his mother was in a jam.")
+  end
+
   def test_it_exists
+    assert_instance_of Joke, @joke
   end
 
   def test_it_has_attributes
+    assert_equal 1, @joke.id
+    assert_equal "Why did the strawberry cross the road?", @joke.setup
+    assert_equal "Because his mother was in a jam.", @joke.punchline
+    binding.pry
   end
 end
