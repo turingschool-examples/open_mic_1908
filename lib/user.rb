57103b2859a6e5ed390621c8005d0eb6ef037905
Tyla Devon <tyladevon@gmail.com>
2019-08-21 09:10:53 -0600

    Add method for learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
+require 'pry'
 
 class User
   attr_reader :name, :jokes
   def initialize(name)
     @name = name
     @jokes = []
-  end 
+  end
+
+  def learn(joke)
+    @joke = joke
+    @jokes << @joke
+  end
 
 end
