733c2005df6a7927118f9f27f1bc4e06da5e715d
Allison E. Moore <m3gacrypt0@gmail.com>
2019-08-21 16:46:08 -0600

    Add iteration 4 implementation

--- lib/openmic.rb
+++ lib/openmic.rb
@@ -7,4 +7,44 @@ class OpenMic
        @performers = []
   end
 
+  def welcome(user)
+       @performers << user
+  end
+
+  def repeated_jokes?
+       # require 'pry' ; binding.pry
+
+       joke_ids = []
+       rep_jokes = false
+
+       if @performers.length > 0
+            @performers.each do |performer|
+                 # binding.pry
+                 performer.each do |user|
+                 # binding.pry
+                      user.jokes each {|joke| joke_ids << joke.id}
+                 # binding.pry
+                 end
+            end
+
+            joke_ids.sort
+            # binding.pry
+
+            joke_ids.each do |id|
+                 # binding.pry
+                 joke_id_last = 0
+                 if id == joke_id_last
+                      rep_jokes = true
+                 else
+                      joke_id_last = id
+                      rep_jokes = false
+                 end
+            end
+       else
+            rep_jokes = false
+       end
+
+       rep_jokes
+  end
+
 end
--- test/openmic_test.rb
+++ test/openmic_test.rb
@@ -11,7 +11,10 @@ class OpenMicTest < Minitest::Test
        @sal = User.new("Sal")
        @ali = User.new("Ali")
 
-       @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+       @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+       @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+       @ali.learn(@joke_1)
+       @ali.learn(@joke_2)
   end
 
   def test_it_exists
@@ -30,5 +33,16 @@ class OpenMicTest < Minitest::Test
 
        @open_mic.welcome(@ali)
        assert_equal [@sal, @ali], @open_mic.performers
+  end
+
+  def test_method_repeated_jokes
+       assert_equal false, @open_mic.repeated_jokes?
+  end
+
+  def test_method_repeated_jokes_true
+       @open_mic.welcome(@sal)
+       @open_mic.welcome(@ali)
+       @ali.tell(@sal, @joke_1)
+       assert_equal true, @open_mic.repeated_jokes?
   end
 end
