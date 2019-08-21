c0f6799ebfd031d9fc7a64ab978399926f09933e
Brian Bower <brian@brianbower.com>
2019-08-21 10:25:51 -0600

    Add iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,14 @@ class User
     @jokes << joke
   end
 
+  def tell(user, joke)
+    user.jokes << joke
+  end
+
+  def joke_by_id(id)
+    #iterate through jokes array and select ones with matching id
+
+    @jokes.find { |joke| joke.id == id }
+  end
+
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -6,7 +6,11 @@ require './lib/user'
 class UserTest < Minitest::Test
 
   def setup
+    # users
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
+
+    # jokes
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
@@ -25,6 +29,20 @@ class UserTest < Minitest::Test
     assert_equal [@joke_1], @sal.jokes
     @sal.learn(@joke_2)
     assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
+
+   def test_tell
+     @sal.learn(@joke_1)
+     @sal.tell(@ali, @joke_1)
+     assert_equal [@joke_1], @ali.jokes
+     @sal.learn(@joke_2)
+     @sal.tell(@ali, @joke_2)
+     assert_equal [@joke_1, @joke_2], @ali.jokes
+   end
+
+  def test_joke_by_id
+    @ali.learn(@joke_1)
+    assert_equal @joke_1, @ali.joke_by_id(1)
   end
 
 end
