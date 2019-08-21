789da225029bfb50dcdc6215c672784781eba29f
Daniel <danmoran.pro@gmail.com>
2019-08-21 10:20:54 -0600

    Complete Iteration  3

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,12 @@ class User
   def learn(joke)
     @jokes << joke
   end
+
+  def tell(user, joke)
+    user.jokes << joke
+  end
+
+  def joke_by_id(id)
+    @jokes.find_all {|joke| joke.id == id}
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -7,6 +7,7 @@ class UserTest < Minitest::Test
 
   def setup
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
@@ -27,7 +28,20 @@ class UserTest < Minitest::Test
     @sal.learn(@joke_1)
     @sal.learn(@joke_2)
     assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
 
+  def test_joke_can_be_told
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+    assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
+  def test_joke_by_id
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+    assert_equal [@joke_1, @joke_2], @ali.jokes
+    assert_equal [@joke_1], @ali.joke_by_id(1)
+    assert_equal [@joke_2], @ali.joke_by_id(2)
   end
 
 
