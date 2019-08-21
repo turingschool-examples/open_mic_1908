ad3e490241163e50424ff115b3b0575bb464764c
David Gomez <davdg25@gmail.com>
2019-08-21 10:07:36 -0600

    last method

--- lib/openmic.rb
+++ lib/openmic.rb
@@ -1,6 +1,14 @@
 require 'pry'
 
 class OpenMic
+  attr_reader :location, :date, :performers
   def initialize(info)
+    @location = info[:location]
+    @date = info[:date]
+    @performers = []
+  end
+
+  def welcome(performer)
+    @performers.push performer
   end
 end
--- test/openmic_test.rb
+++ test/openmic_test.rb
@@ -8,9 +8,29 @@ require 'pry'
 class OpenMicTest < Minitest::Test
   def setup
     @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @sal = User.new("Sal")
+    @ali = User.new("Ali")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+
   end
 
   def test_existence
     assert_instance_of OpenMic, @open_mic
   end
+
+  def test_attributes
+    assert_equal "Comedy Works", @open_mic.location
+    assert_equal "11-20-18", @open_mic.date
+    assert_equal [], @open_mic.performers
+  end
+
+  def test_welcome_and_perfomers_methods
+    @open_mic.welcome(@sal)
+    @open_mic.welcome(@ali)
+    assert_equal [@sal, @ali], @open_mic.performers
+  end
+
+  def test_repeated_jokes
 end
