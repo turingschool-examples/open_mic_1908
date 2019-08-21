cb3af5720e8e6b1cab112091c1acc74254af094f
Ryan Hantak <rhantak@gmail.com>
2019-08-21 09:37:09 -0600

    Update user class file and matching test file to Iteration 3 specifications

--- lib/user.rb
+++ lib/user.rb
@@ -11,4 +11,12 @@ class User
   def learn(new_joke)
     @jokes << new_joke
   end
+
+  def tell(other_user, joke)
+    other_user.jokes << joke
+  end
+
+  def joke_by_id(id)
+    @jokes.find { |joke| joke.id == id}
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -8,26 +8,48 @@ class UserTest < Minitest::Test
   def setup
     @joke1 = Joke.new(1,"Set","Punch")
     @joke2 = Joke.new(2,"Setup2","Punchline2")
-    @user = User.new("Ryan")
+    @user1 = User.new("Ryan")
+    @user2 = User.new("Jim Bob")
   end
 
   def test_it_exists
-    assert_instance_of User, @user
+    assert_instance_of User, @user1
   end
 
   def test_it_has_a_name
-    assert_equal "Ryan", @user.name
+    assert_equal "Ryan", @user1.name
   end
 
   def test_it_starts_with_no_jokes
-    assert_equal [], @user.jokes
+    assert_equal [], @user1.jokes
   end
 
   def test_it_can_learn_jokes
-    @user.learn(@joke1)
-    @user.learn(@joke2)
+    @user1.learn(@joke1)
+    @user1.learn(@joke2)
 
-    assert_equal [@joke1,@joke2], @user.jokes
+    assert_equal [@joke1,@joke2], @user1.jokes
   end
+
+  def test_it_can_tell_jokes_to_other_users
+    @user1.learn(@joke1)
+    @user1.learn(@joke2)
+    @user1.tell(@user2, @joke1)
+
+    assert_equal [@joke1], @user2.jokes
+
+    @user1.tell(@user2 ,@joke2)
+
+    assert_equal [@joke1, @joke2], @user2.jokes
+  end
+
+  def test_it_can_find_jokes_by_id
+    @user1.learn(@joke1)
+    @user1.learn(@joke2)
+
+    assert_equal @joke1, @user1.joke_by_id(1)
+    assert_equal @joke2, @user1.joke_by_id(2)
+  end
+
 
 end
