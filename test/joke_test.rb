215694ddc7286718c2221400e41545b8e474d769
Tyla Devon <tyladevon@gmail.com>
2019-08-21 09:10:53 -0600

    Add test for existence joke instance

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -3,9 +3,16 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
-  def test_it_exists
+
+  def setup
+    @joke = Joke.new
   end
 
-  def test_it_has_attributes
+
+  def test_it_exists
+    assert_instance_of Joke, @joke
   end
+
+  # def test_it_has_attributes
+  # end
 end
