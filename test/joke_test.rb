4b16aa3fa15d004a6d2fffe472bc3c5c02323c6c
Ryan Hantak <rhantak@gmail.com>
2019-08-21 09:10:25 -0600

    Add joke class and matching test file

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -3,9 +3,18 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
+
+  def setup
+    @joke = Joke.new(1, "Why did the chicken cross the road?", "To get to the other side.")
+  end
+
   def test_it_exists
+    assert_instance_of Joke, @joke
   end
 
   def test_it_has_attributes
+    assert_equal 1, @joke.id
+    assert_equal "Why did the chicken cross the road?", @joke.setup
+    assert_equal "To get to the other side.", @joke.punchline
   end
 end
