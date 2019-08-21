bed17268ec13aaac3a1481bda201cb84e16a3310
John Travers <johnk.travers@gmail.com>
2019-08-21 09:24:09 -0600

    Complete iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,14 @@ class User
     @jokes.push(joke)
   end
 
+  def tell(user, joke)
+    user.learn(joke)
+  end
+
+  def joke_by_id(id)
+    @jokes.find do |joke|
+      joke.id == id
+    end
+  end
+
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -10,6 +10,7 @@ class UserTest < Minitest::Test
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
 
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
   end
 
   def test_it_exists
@@ -28,6 +29,31 @@ class UserTest < Minitest::Test
     @sal.learn(@joke_2)
 
     assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
+
+  def test_it_can_tell_a_joke_to_another_user
+    @sal.tell(@ali, @joke_1)
+
+    assert_equal [@joke_1], @ali.jokes
+
+    @sal.tell(@ali, @joke_2)
+
+    assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
+  def test_it_can_list_learned_jokes_by_id
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+
+    assert_equal @joke_1, @ali.joke_by_id(1)
+    assert_equal @joke_2, @ali.joke_by_id(2)
+  end
+
+  def test_joke_not_yet_learned_returns_nil
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+
+    assert_nil @ali.joke_by_id(3)
   end
 
 end
