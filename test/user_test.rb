820dfd215a233cd0cfae8f7b9a0dff35a3c0e2d6
Allison E. Moore <m3gacrypt0@gmail.com>
2019-08-21 15:39:33 -0600

    Update user_test.rb for iteration 3 specifications

--- test/user_test.rb
+++ test/user_test.rb
@@ -6,6 +6,7 @@ require './lib/user'
 class UserTest < Minitest::Test
   def setup
        @sal = User.new("Sal")
+       @ali = User.new("Ali")
   end
 
   def test_it_exists
@@ -25,6 +26,30 @@ class UserTest < Minitest::Test
       @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
       @sal.learn(@joke_2)
       assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
+
+  def test_method_tell
+       @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+       @sal.learn(@joke_1)
+       @sal.tell(@ali, @joke_1)
+       assert_equal [@joke_1], @ali.jokes
+
+       @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+       @sal.learn(@joke_2)
+       @sal.tell(@ali, @joke_2)
+       assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
+  def test_method_joke_by_id
+       @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+       @sal.learn(@joke_1)
+       @sal.tell(@ali, @joke_1)
+       assert_equal @joke_1, @ali.joke_by_id(1)
+
+       @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+       @sal.learn(@joke_2)
+       @sal.tell(@ali, @joke_2)
+       assert_equal @joke_2, @ali.joke_by_id(2)
   end
 
 end
