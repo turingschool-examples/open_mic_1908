03c85bc0c333619496d0a39b8db62eb40a99e031
Jon Patterson <JonPatt92@gmail.com>
2019-08-21 09:17:56 -0600

    created user.rb & user_test.rb. Built tests for iteration 2 user_test.rb file.

---
+++ test/user_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require './lib/user'
+
+class UserTest < Minitest::Test
+  def setup
+    @sal = User.new("Sal")
+  end
+
+  def test_it_exists
+
+    assert_instance_of User, @sal
+  end
+
+  def test_it_has_attributes
+
+    assert_equal "Sal", @sal.name
+    assert_equal [], @sal.jokes
+  end
