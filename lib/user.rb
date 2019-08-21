7829f1b6ea52b33fff11c5199c68b838f3f6efd9
Mary Lang <mcl86@cornell.edu>
2019-08-21 09:33:49 -0600

    Add iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,16 @@ attr_reader :name, :jokes
     @jokes << joke
   end
 
+  def tell(user, joke)
+    user.learn(joke)
+  end
+
+  def joke_by_id(id)
+    selected = []
+    @jokes.each do |joke|
+      selected << joke if joke.id == id
+    end
+    selected[0]
+  end
+
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -9,6 +9,7 @@ class UserTest < MiniTest::Test
     @user = User.new("Sal")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    @user_2 = User.new("Ali")
   end
 
   def test_it_exists
@@ -25,5 +26,19 @@ class UserTest < MiniTest::Test
     @user.learn(@joke_2)
     assert_equal [@joke_1,@joke_2], @user.jokes
   end
+
+  def test_tell
+    @user.tell(@user_2, @joke_1)
+    @user.tell(@user_2, @joke_2)
+    assert_equal [@joke_1,@joke_2], @user_2.jokes
+  end
+
+  def test_joke_by_id
+    @user.tell(@user_2, @joke_1)
+    @user.tell(@user_2, @joke_2)
+    assert_equal @joke_1, @user_2.joke_by_id(1)
+    assert_equal @joke_2, @user_2.joke_by_id(2)
+  end
+
 
 end
