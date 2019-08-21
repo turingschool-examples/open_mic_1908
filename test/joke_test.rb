aa79667b11850e8e7568f3c50a9aa5d0cc4cdc35
Michael Cooper <macoop2363@mgmail.com>
2019-08-21 09:15:52 -0600

    Removed unnecessary comments

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -2,21 +2,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
 
-
-# pry(main)> require './lib/joke'
-# # => true
-
-# pry(main)> joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")    
-# # => #<Joke:0x00007f84602e4190...>
-
-# pry(main)> joke.id
-# # => 1
-
-# pry(main)> joke.setup
-# # => "Why did the strawberry cross the road?"
-
-# pry(main)> joke.punchline
-# # => "Because his mother was in a jam."
 class JokeTest < Minitest::Test
   
   def setup 
