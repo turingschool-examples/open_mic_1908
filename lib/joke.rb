e6326f10740e1d87bece76f5c26c96bffc374597
Tyla Devon <tyladevon@gmail.com>
2019-08-21 09:10:53 -0600

    Add method for punchline
    Co-authored-by: David Lopez <ldavid.x.lopezl@gmail.com>

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,11 +1,16 @@
 class Joke
-  attr_reader :id, :question
+  attr_reader :id, :question, :answer
   def initialize(id, question, answer)
     @id = id
     @question = question
+    @answer = answer
   end
 
   def setup
-    @question 
+    @question
+  end
+
+  def punchline
+    @answer
   end
 end
