e5d9a433d8902dc279cf07f9f6e30d384bad56c2
Brian Bower <brian@brianbower.com>
2019-08-21 09:10:48 -0600

    Add iteration 1

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -3,9 +3,19 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
+
+  def setup
+    @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+  end
+
   def test_it_exists
+    assert_instance_of Joke, @joke
   end
 
   def test_it_has_attributes
+    assert_equal 1, @joke.id
+    assert_equal "Why did the strawberry cross the road?", @joke.setup
+    assert_equal "Because his mother was in a jam.", @joke.punchline
   end
+
 end
