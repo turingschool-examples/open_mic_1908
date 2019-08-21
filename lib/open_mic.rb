0ed799e19934fa7e286c626898189bfc83d8cc08
Alice Post <ap2322@gmail.com>
2019-08-21 10:29:30 -0600

    Add repeated_jokes? method and test.

---
+++ lib/open_mic.rb
@@ -0,0 +1,44 @@
+class OpenMic
+  def initialize(event)
+    @event = {
+      location: event[:location],
+      date: event[:date],
+      performers: [],
+    }
+  end
+
+  def location
+    @event[:location]
+  end
+
+  def date
+    @event[:date]
+  end
+
+  def performers
+    @event[:performers]
+  end
+
+  def welcome(user)
+    @event[:performers] << user
+  end
+
+  def repeated_jokes?
+    jokes_told = @event[:performers].map do |performer|
+      performer.jokes
+    end
+
+    # if an array of joke ids == array of unique joke ids, then no repeats
+    # if an array of joke ids != array of uniq joke ids, then yes repeats
+    joke_ids = jokes_told.flatten.map do |joke|
+      joke.id
+    end
+
+    if joke_ids.length == joke_ids.uniq.length
+      return false
+    else
+      return true
+    end
+  end
+  
+end
