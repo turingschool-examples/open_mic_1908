3ebe6618f1c24baf4feaedfa285a34145a16df6d
Graham Thompson <grwthomps@gmail.com>
2019-08-21 09:38:12 -0600

    Add tests for iteration 3 interaction pattern

--- test/user_test.rb
+++ test/user_test.rb
@@ -7,6 +7,7 @@ class UserTest < Minitest::Test
 
   def setup
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
@@ -25,6 +26,21 @@ class UserTest < Minitest::Test
     @sal.learn(@joke_2)
     assert_equal @joke_1, @sal.jokes[0]
     assert_equal @joke_2, @sal.jokes[1]
+  end
+
+  def test_sal_can_tell_ali_jokes
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+    assert_equal [], @ali.jokes
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+    assert_equal @joke_1, @ali.jokes[0]
+    assert_equal @joke_2, @ali.jokes[1]
+  end
+
+  def test_ali_joke_by_id
+    assert_equal @joke_1, @ali.joke_by_id(1)
+    assert_equal @joke_2, @ali.joke_by_id(2)
   end
 
 end
