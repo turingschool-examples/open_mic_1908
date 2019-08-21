31d1bf8322ec5602d8c45e6837f0a5da1a98338c
Tyla Devon <tyladevon@gmail.com>
2019-08-21 09:10:53 -0600

    Add test for Sal learn joke

--- test/user_test.rb
+++ test/user_test.rb
@@ -2,11 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
 require './lib/user'
+require 'pry'
 
 class UserTest < Minitest::Test
 
   def setup
     @sal = User.new("Sal")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_user_exists
@@ -19,5 +22,12 @@ class UserTest < Minitest::Test
 
   def test_user_has_jokes
     assert_equal [], @sal.jokes
-  end 
+  end
+
+  def test_sal_learn_joke
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+
+    assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
 end
