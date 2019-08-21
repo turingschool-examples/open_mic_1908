83993e7f483c2ac917444a798c3a6aa91da6ebcd
Laura Schulz <lrs8810@gmail.com>
2019-08-21 10:39:47 -0600

    Add welcome method and passing test

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,10 +1,14 @@
 class OpenMic
   attr_reader :location, :date, :performers
 
-  def initialize (location: "Comedy Works", date: "11-20-18", performers: [])
+  def initialize (location: "Comedy Works", date: "11-20-18")
     @location = location
     @date = date
     @performers = []
+  end
+
+  def welcome(user)
+    @performers << user
   end
 
 end
--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -6,6 +6,9 @@ require './lib/open_mic'
 
 class OpenMicTest < Minitest::Test
   def setup
+    @sal = User.new("Sal")
+    @ali = User.new("Ali")
+
     @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
   end
 
@@ -17,5 +20,12 @@ class OpenMicTest < Minitest::Test
     assert_equal "Comedy Works", @open_mic.location
     assert_equal "11-20-18", @open_mic.date
     assert_equal [], @open_mic.performers
+  end
+
+  def test_it_welcomes_new_performers
+    @open_mic.welcome(@sal)
+    @open_mic.welcome(@ali)
+
+    assert_equal [@sal, @ali], @open_mic.performers
   end
 end
