d183c352ae6d64f8438331c037af761021f40ebb
Tyla Devon <tyladevon@gmail.com>
2019-08-21 09:10:53 -0600

    Add test for User name
    Co-authored-by: David Lopez <ldavid.x.lopezl@gmail.com>

--- test/user_test.rb
+++ test/user_test.rb
@@ -3,4 +3,18 @@ require 'minitest/pride'
 require './lib/joke'
 require './lib/user'
 
-class UserTest < Minitest::Test 
+class UserTest < Minitest::Test
+
+  def setup
+    @sal = User.new("Sal")
+  end
+
+  def test_user_exists
+    assert_instance_of User, @sal
+  end
+
+  def test_user_has_a_name
+    assert_equal "Sal", @sal.name 
+
+  end
+end
