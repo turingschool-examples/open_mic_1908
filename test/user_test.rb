78220e6702a8e5346e06697c74e13d51763cb54c
Tyla Devon <tyladevon@gmail.com>
2019-08-21 09:10:53 -0600

    Add test for Ali jokes

--- test/user_test.rb
+++ test/user_test.rb
@@ -10,14 +10,17 @@ class UserTest < Minitest::Test
     @sal = User.new("Sal")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    @ali = User.new("Ali")
   end
 
   def test_user_exists
     assert_instance_of User, @sal
+    assert_instance_of User, @ali
   end
 
   def test_user_has_a_name
     assert_equal "Sal", @sal.name
+    assert_equal "Ali", @ali.name
   end
 
   def test_user_has_jokes
@@ -36,6 +39,13 @@ class UserTest < Minitest::Test
     @sal.learn(@joke_2)
 
     assert_equal [@joke_1, @joke_2], @sal.jokes
-
   end
+
+  def test_ali_jokes
+      @ali.sal_tells(@ali, @joke_1)
+      @ali.sal_tells(@ail, @joke_2)
+
+      assert_equal [@joke_1, @joke_2], @ali.jokes
+  end
+
 end
