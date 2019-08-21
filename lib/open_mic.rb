9ef6a8c9762a5da3082be2f120d8cc6651c1c84f
Laura Schulz <lrs8810@gmail.com>
2019-08-21 10:34:10 -0600

    Initialize open mic

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,2 +1,14 @@
 class OpenMic
+  attr_reader :location, :date, :performers
+
+  def initialize (location: "Comedy Works", date: "11-20-18", performers: [])
+    @location = location
+    @date = date
+    @performers = []
+  end
+
 end
+
+# def initialize(name, category = :appetizer)
+#   @name = name
+#   @category = {category => @name}
--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -6,5 +6,16 @@ require './lib/open_mic'
 
 class OpenMicTest < Minitest::Test
   def setup
-  end 
+    @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+  end
+
+  def test_it_exists
+    assert_instance_of OpenMic, @open_mic
+  end
+
+  def test_it_has_attributes
+    assert_equal "Comedy Works", @open_mic.location
+    assert_equal "11-20-18", @open_mic.date
+    assert_equal [], @open_mic.performers
+  end
 end
